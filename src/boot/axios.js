1730e99aa109e02ea141305611a6388cab5b46cf
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-01-17 08:37:25 +0700

    kantor seep

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -5,8 +5,8 @@ import { notifErr } from 'src/modules/utils'
 import { setCssVar } from 'quasar'
 
 // const SERV = 'http://localhost/api.webrsudmochsaleh/public'// rumah
-const SERV = 'http://localhost/api.webrsudmohsaleh/public'// kantor
-// const SERV = 'https://rsudmochsaleh.my.id'
+// const SERV = 'http://localhost/api.webrsudmohsaleh/public'// kantor
+const SERV = 'https://rsudmochsaleh.my.id'
 const SERVER = SERV + '/api'
 
 const api = axios.create({ baseURL: SERVER })
