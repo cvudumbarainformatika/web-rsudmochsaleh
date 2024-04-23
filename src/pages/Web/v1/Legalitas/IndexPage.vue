bc8be62244f75ad77181b8d20561dbf1837336b9
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-04-23 20:31:53 +0700

    sfdsf

--- src/pages/Web/v1/Legalitas/IndexPage.vue
+++ src/pages/Web/v1/Legalitas/IndexPage.vue
@@ -1,25 +1,52 @@
 <template>
   <q-page class="fit column flex-center bg-dark text-white">
-    <div class="text-subtitle">
-      Dokumen ini dikeluarkan oleh
+    <div
+      v-if="store.loading"
+      class="column flex-center"
+    >
+      <q-spinner-dots
+        color="white"
+        size="2em"
+      />
+      <div>Harap Tunggu</div>
     </div>
-    <div class="text-subtitle2 text-weight-bold">
-      {{ app?.header?.title }}
-    </div>
-    <div class="text-subtitle text-weight-bold">
-      KOTA PROBOLINGGO
-    </div>
-    <div class="text-subtitle">
-      ditandatangani secara Electronik oleh
-    </div>
-    <div class="text-subtitle text-weight-bold">
-      {{ store.data?.datasimpeg?.nama ?? 'INVALID' }}
-    </div>
-    <div class="text-subtitle">
-      ID {{ store.data?.datasimpeg?.nip ?? store.data?.datasimpeg?.nik ?? '-' }}
-    </div>
-    <div class="text-subtitle">
-      {{ store.data?.rs3 ?? '-' }}
+    <div
+      v-else
+      class="column flex-center"
+    >
+      <div
+        v-if="!store.isError"
+        class="text-subtitle"
+      >
+        Dokumen ini dikeluarkan oleh
+      </div>
+      <div
+        v-if="!store.isError"
+        class="text-subtitle2 text-weight-bold"
+      >
+        {{ app?.header?.title }}
+      </div>
+      <div
+        v-if="!store.isError"
+        class="text-subtitle text-weight-bold q-mb-lg"
+      >
+        KOTA PROBOLINGGO
+      </div>
+      <div
+        v-if="!store.isError"
+        class="text-subtitle"
+      >
+        ditandatangani secara Electronik oleh
+      </div>
+      <div class="text-subtitle text-weight-bold">
+        {{ store.data?.datasimpeg?.nama ?? 'INVALID' }}
+      </div>
+      <div class="text-subtitle">
+        ID {{ store.data?.datasimpeg?.nip ?? store.data?.datasimpeg?.nik ?? '-' }}
+      </div>
+      <div class="text-subtitle">
+        {{ store.data?.rs3 ?? '-' }}
+      </div>
     </div>
   </q-page>
 </template>
