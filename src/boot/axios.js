eb2d384c81bc661843e0fb5d0f94740487570bfa
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-07-30 20:37:09 +0700

    rev: deploy tt

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -5,8 +5,7 @@ import { useAuthStore } from 'src/stores/auth'
 import { notifErr } from 'src/modules/utils'
 import { setCssVar } from 'quasar'
 
-// const SERV = 'http://localhost/api.webrsudmochsaleh/public' // rumah
-// const SERV = 'http://localhost/api.webrsudmohsaleh/public' // kantor
+
 const SERV = 'https://rsudmochsaleh.my.id'
 const SERVER = SERV + '/api'
 
